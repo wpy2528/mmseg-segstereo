c94575dbf20f9bcde93facea7e9c2290ad75f39b
城宽 马 <machengkuan@ldrobot.com>
2025-07-31 16:18:59 +0800

    [exp] 0731

--- mmseg/datasets/ld_perception_seg_dataset.py
+++ mmseg/datasets/ld_perception_seg_dataset.py
@@ -47,13 +47,19 @@ class LDPerceptionSegDataset(BaseSegDataset):
         assert include is None or exclude is None, "include和exclude不能同时存在"
         if include is not None:
             assert isinstance(include, list), "include必须为list"
-            include = ['/' + e.rstrip('/').lstrip('/') + '/' for e in include]
+            include = ['/' + e.rstrip('/').lstrip('/') + '/' if (not e.endswith('.txt')) else e for e in include]
         if exclude is not None:
             assert isinstance(exclude, list), "exclude必须为list"
-            exclude = ['/' + e.rstrip('/').lstrip('/') + '/' for e in exclude]
+            exclude = ['/' + e.rstrip('/').lstrip('/') + '/' if (not e.endswith('.txt')) else e for e in exclude]
         if repeat is not None:
             assert isinstance(repeat, dict), "repeat必须为int"
-            self.repeat = repeat
+            r_ = {}
+            for key, value in repeat.items():
+                if not key.endswith('.txt'):
+                    r_['/' + key.rstrip('/').lstrip('/') + '/'] = value
+                else:
+                    r_[key] = value
+            self.repeat = r_
         self.include = include
         self.exclude = exclude
         self.repeat = repeat
@@ -85,19 +91,37 @@ class LDPerceptionSegDataset(BaseSegDataset):
             assert os.path.isdir(self.data_root), self.data_root
             lines = glob.glob(os.path.join(self.data_root, "**", "images", "*.jpg"), recursive=True)
 
+        # 进行数据集增删时，允许给定的关键字是数据路径中的子串，也可以是txt文件路径。
+        # 如果是子串，那么对包含子串的数据路径进行处理；如果是txt文件路径，那么直接读取txt中的内容，对这些内容进行对应的处理。
         if self.include is not None:
-            lines = [line for line in lines if any(include in line for include in self.include)]
+            txt_paths = [e for e in self.include if e.endswith('.txt')]
+            keywords = [e for e in self.include if not e.endswith('.txt')]
+            lines = [line for line in lines if any(include in line for include in keywords)]
+            for txt_path in txt_paths:
+                with open(txt_path, 'r') as f:
+                    lines.extend([line.strip() for line in f.readlines()])
         if self.exclude is not None:
-            lines = [line for line in lines if not any(exclude in line for exclude in self.exclude)]
+            txt_paths = [e for e in self.exclude if e.endswith('.txt')]
+            keywords = [e for e in self.exclude if not e.endswith('.txt')]
+            lines = set([line for line in lines if not any(exclude in line for exclude in keywords)])
+            exclude_paths = set()
+            for txt_path in txt_paths:
+                with open(txt_path, 'r') as f:
+                    exclude_paths.update([line.strip() for line in f.readlines()])
+            # 这里需要转set加速筛除
+            lines = list(set(lines) - exclude_paths)
         if self.repeat is not None:
             # 根据repeat的dict，对lines进行重复
             repeat_lines = []
             for line in lines:
                 for key, value in self.repeat.items():
-                    if key in line:
+                    if (not key.endswith('.txt')) and (key in line):
                         repeat_lines.extend([line] * value)
                         break
-
+            txt_paths = [e for e in self.repeat.keys() if e.endswith('.txt')]
+            for txt_path in txt_paths:
+                with open(txt_path, 'r') as f:
+                    repeat_lines.extend([line.strip() for line in f.readlines()] * self.repeat[txt_path])
             lines += repeat_lines
 
         src_log_path = MMLogger.get_current_instance().log_file
